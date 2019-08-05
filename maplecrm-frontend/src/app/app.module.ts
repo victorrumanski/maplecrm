1b48efbdaf2b603d4b970779d388fe9c02e0e149
Victor Rumanski <victor.rumanski@gmail.com>
2019-08-05 09:00:00 +0100

    first components

--- maplecrm-frontend/src/app/app.module.ts
+++ maplecrm-frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ForgotComponent } from './forgot/forgot.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgotComponent
   ],
   imports: [
     BrowserModule,
