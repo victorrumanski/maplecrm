f859d52c892a10c95254e49a23e8358b59370a07
Victor Rumanski <victor.rumanski@gmail.com>
2019-08-08 09:16:51 +0100

    oK

---
+++ maplecrm-backend/src/main/java/com/maplecrm/maplecrmapi/MaplecrmapiApplication.java
@@ -0,0 +1,13 @@
+package com.maplecrm.maplecrmapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaplecrmapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaplecrmapiApplication.class, args);
+	}
+
+}
